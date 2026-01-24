87438b00be14b17226a8bee5547393eb3672af07
John <babayagagunfu@gmail.com>
2026-01-25 02:09:06 +0530

    Add RFM scoring and customer segmentation (Week 2)

---
+++ python/rfm_analysis.py
@@ -0,0 +1,95 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+print("CURRENT WORKING DIRECTORY:", os.getcwd())
+# 1. Connect to MySQL
+engine = create_engine(
+    "mysql+pymysql://<username>:<password>@localhost/consumer360"
+) # Replace with local MySQL credentials (URL-encode password if needed)
+
+# 2. Load customer-level data (like Excel)
+df = pd.read_sql("SELECT * FROM single_customer_view", engine)
+
+# 3. RFM scoring using quintiles (1–5)
+
+# Recency: lower is better (will invert later)
+df['R_bin'] = pd.qcut(
+    df['recency_days'],
+    q=5,
+    duplicates='drop'
+)
+
+# Frequency: higher is better
+df['F_bin'] = pd.qcut(
+    df['frequency'],
+    q=5,
+    duplicates='drop'
+)
+
+# Monetary: higher is better
+df['M_bin'] = pd.qcut(
+    df['monetary'],
+    q=5,
+    duplicates='drop'
+)
+
+# 4. Convert bins to numeric scores
+
+# Convert categorical bins to numbers and cat.codes gives 0,1,2,... in increasing order
+
+# Recency needs inversion (lower days = better score)
+df['R_score'] = df['R_bin'].cat.codes.max() - df['R_bin'].cat.codes + 1
+
+# Frequency & Monetary increase naturally
+df['F_score'] = df['F_bin'].cat.codes + 1
+df['M_score'] = df['M_bin'].cat.codes + 1
+
+# 5. RFM Code
+
+df['RFM_code'] = (
+    df['R_score'].astype(str) +
+    df['F_score'].astype(str) +
+    df['M_score'].astype(str)
+)
+
+# 6. Simple segmentation
+def segment(row):
+    R = int(row['R_score'])
+    F = int(row['F_score'])
+    M = int(row['M_score'])
+
+    if R >= 4 and F >= 4 and M >= 4:
+        return 'Champions'
+
+    elif R >= 3 and F >= 4 and M >= 3:
+        return 'Loyal'
+
+    elif R >= 4 and F in [2, 3] and M in [2, 3]:
+        return 'Potential Loyalist'
+
+    elif R >= 3 and F >= 3 and M <= 2:
+        return 'Price Sensitive'
+
+    elif R in [2, 3] and F in [2, 3] and M in [2, 3]:
+        return 'Needs Attention'
+
+    elif R == 2 and F <= 2:
+        return 'About to Sleep'
+
+    elif R == 1 and F in [1, 2]:
+        return 'Hibernating'
+
+    else:
+        return 'Lost'
+    
+# 7. Segmentation  
+df['RFM_segment'] = df.apply(segment, axis=1)
+    
+# 8. Save output
+df.to_csv(
+"/Users/johnhanma/Documents/Git/Infotact Solutions Data Analyst Projects/Consumer360/python/rfm_output.csv",
+    index=False
+)
+
+print("RFM analysis completed.")
